18c85752ffcb4ffc8e3d73a8a77bfe92c14660c0
yjineey <did3706@naver.com>
2025-07-27 03:48:36 +0900

    SNS 아이콘 배포 시 경로 오류 수정 (imgPath 적용)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,7 @@
 import Link from 'next/link'
 import { imgPath } from '@/lib/imgPath'
 
-import {
-  BookOpenText,
-  NotebookPen,
-  HomeIcon,
-  UserIcon,
-  Menu,
-} from 'lucide-react'
+import { BookOpenText, Heart, HomeIcon, UserIcon, Menu } from 'lucide-react'
 
 export const SNSItems = [
   {
@@ -35,7 +29,7 @@ export const MobileItems = [
   {
     label: '큐티',
     href: '/bible/qt',
-    icon: NotebookPen,
+    icon: Heart,
   },
   {
     label: '홈',
@@ -48,8 +42,8 @@ export const MobileItems = [
     icon: UserIcon,
   },
   {
-    label: '더보기',
-    href: '#',
+    label: '메뉴',
+    href: '/more/menu',
     icon: Menu,
   },
 ]
@@ -58,17 +52,17 @@ export default function Footer() {
 
   return (
     <>
-      <div className="hidden md:flex item-center justify-center p-2">
+      <div className="hidden md:flex item-center justify-center p-3 border-t gap-4">
         <img
-          src={imgPath(`/logos/nw_sm_logo.png`)}
+          src={imgPath(`/logos/nw_logo.png`)}
           alt="nw-footer-logo"
-          className="w-48 h-auto invert dark:invert-0"
+          className="w-72 h-16 invert-0 dark:invert"
         />
         <div className="space-y-1 text-left">
           <p className="font-semibold">뉴웨이교회</p>
-          <p>주소 | 서울특별시 강서구 화곡로 64길 67</p>
-          <p>전화 | 070-4047-0478</p>
-          <p>이메일 | newwaychurchkr@gmail.com</p>
+          {/* <p>주소 | 서울특별시 강서구 화곡로 64길 67</p> */}
+          {/* <p>전화 | 070-4047-0478</p> */}
+          {/* <p>이메일 | newwaychurchkr@gmail.com</p> */}
           <div className="flex gap-4">
             {SNSItems.map(({ label, href, icon: Icon }) => (
               <Link
@@ -77,7 +71,11 @@ export default function Footer() {
                 className="flex items-center gap-2 hover-link"
                 target="_blank"
               >
-                <img src={Icon} alt={label} className="w-4 h-4 dark:invert" />
+                <img
+                  src={imgPath(Icon)}
+                  alt={label}
+                  className="w-4 h-4 dark:invert"
+                />
                 <span>{label}</span>
               </Link>
             ))}
@@ -85,7 +83,7 @@ export default function Footer() {
           <p>ⓒ {year} NEWWAY CHURCH. All rights reserved.</p>
         </div>
       </div>
-      <div className="fixed md:hidden bottom-0 left-0 right-0 h-12">
+      <div className="fixed md:hidden h-16 bottom-0 left-0 right-0 p-4 border-t bg-neutral-900 text-gray-200">
         <div className="flex">
           {MobileItems.map(({ href, icon: Icon }) => (
             <Link
