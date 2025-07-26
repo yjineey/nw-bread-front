c26a616cc0dc5ae47330c76b9bc7b990de1eb160
yjineey <did3706@naver.com>
2025-07-26 16:34:13 +0900

    tailwindcss 다크모드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // 다크모드 설정 (클래스 기반)
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
